ff17b7279bd2dbe5388935de91d838258efbebda
Ahmad Moussa <ahmad.moussa@kosmopolead.com>
2019-02-18 09:15:11 +0100

    Add edit, update, show and index listing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'welcome#home'
+  root 'articles#index'
   # get 'welcome/home', to: 'welcome#home'
   get 'about', to: 'welcome#about'
 
